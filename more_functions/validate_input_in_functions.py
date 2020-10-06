262e5e9e7c4b7ad8a2ec4a74333cd9f723315151
Vicka93 <57420845+Vicka93@users.noreply.github.com>
2020-10-06 18:46:04 -0500

    failed test validate input

---
+++ more_functions/validate_input_in_functions.py
@@ -0,0 +1,20 @@
+"""
+Author:Viktoria Denys
+Program:validate_input_in_functions.py
+
+takes a test_name, test_score,
+and invalid_message that validates the test_score, then prints valid input as 'Test name: ##'.
+
+"""
+
+
+def score_input(test_name, test_score=0, invalid_message="Invalid test score, try again!"):
+    """takes a test_name, test_score,
+    and invalid_message that validates the test_score,
+    then prints valid input as 'Test name: ##'."""
+    print("{}: {}".format(test_name, test_score))
+    return "pass"
+
+
+if __name__ == '__main__':
+    score_input("test1", 45)
---
+++ test_functions/test_valid_input_in_functions.py
@@ -0,0 +1,36 @@
+"""
+Author:Viktoria Denys
+Program:validate_input_in_functions.py
+
+takes a test_name, test_score,
+and invalid_message that validates the test_score, then prints valid input as 'Test name: ##'.
+
+"""
+import unittest
+from more_functions import validate_input_in_functions as valide_input
+
+
+class MyTestCase(unittest.TestCase):
+    def test_score_input_test_name(self):
+        self.assertEqual("Python1!: 0", valide_input.score_input("Python1!"))
+    """
+    def test_score_input_test_score_valid(self):
+        self.assertEqual(True, False)
+
+    def test_score_input_test_score_below_range(self):
+        self.assertEqual(True, False)
+
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual(True, False)
+
+    def test_test_score_non_numeric(self):
+        self.assertEqual(True, False)
+
+    def test_score_input_invalid_message(self):
+        self.assertEqual(True, False)
+
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
